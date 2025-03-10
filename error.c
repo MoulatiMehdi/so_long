20e2e7c542f5775af2a4e997e19f9faa95e83b5b
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-03-10 23:26:37 +0000

    add red color for error

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:30:51 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/03/10 13:30:51 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/03/10 23:20:01 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "so_long.h"
 #include <stdio.h>
 
-#define ERR_MSG "Error\n"
+#define ERR_MSG "\033[1;31mError\n\t"
 
 void	ft_perror(char *str)
 {
@@ -28,21 +28,22 @@ void	ft_map_error(t_map **map, t_map_state state)
 		return ;
 	ft_putstr_fd(ERR_MSG, 2);
 	if (state == ERR_FILENAME_INVALID)
-		ft_putstr_fd("Invalid filename.\n", 2);
+		ft_putstr_fd("Invalid filename.", 2);
 	if (state == ERR_MAP_CHAR_INVALID)
-		ft_putstr_fd("The map contains an invalid character.\n", 2);
+		ft_putstr_fd("The map contains an invalid character.", 2);
 	if (state == ERR_MAP_COLLECTABLE)
-		ft_putstr_fd("The map must contain at least one collectable.\n", 2);
+		ft_putstr_fd("The map must contain at least one collectable.", 2);
 	if (state == ERR_MAP_NOT_CLOSED)
-		ft_putstr_fd("The map is not closed.\n", 2);
+		ft_putstr_fd("The map is not closed.", 2);
 	if (state == ERR_MAP_PATH_INVALID)
-		ft_putstr_fd("The map is not solvable.\n", 2);
+		ft_putstr_fd("The map is not solvable.", 2);
 	if (state == ERR_MAP_NOT_RECTANGLE)
-		ft_putstr_fd("The map must be rectangular.\n", 2);
+		ft_putstr_fd("The map must be rectangular.", 2);
 	if (state == ERR_MAP_ONE_EXIT)
-		ft_putstr_fd("The map must contain exactly one exit.\n", 2);
+		ft_putstr_fd("The map must contain exactly one exit.", 2);
 	if (state == ERR_MAP_ONE_PLAYER)
-		ft_putstr_fd("The map must contain exactly one player.\n", 2);
+		ft_putstr_fd("The map must contain exactly one player.", 2);
+	ft_putstr_fd("\n\033[0m", 2);
 	ft_map_destroy(map);
 	exit(1);
 }
